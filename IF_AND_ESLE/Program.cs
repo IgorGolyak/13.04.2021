ee352ff9e0b9e47e38cb11e2c4f1c1b1a3aef149
Игорь Голяк <golyak.03@inbox.ru>
2021-04-13 11:38:48 +0700

    13.04.2021

---
+++ IF_AND_ESLE/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IF_AND_ESLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число c");
+            double c = Convert.ToDouble(Console.ReadLine());
+            if ((a < b + c) & (b < a + c) & (c < a + b))
+                {
+                Console.WriteLine("труг.сущ");
+                }
+            else
+            {
+                Console.WriteLine("Треуг.не сущ.");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
